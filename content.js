f290174bdd7ecb1b384289fb67adc2179f479d1c
Mikhail Valentsev <michael@valentsev.ru>
2023-10-31 14:57:14 +0500

    Nothing important

--- content.js
+++ content.js
@@ -1,35 +1,34 @@
 const url = 'https://iss.moex.com/iss/engines/stock/markets/index/securities/TRURA.json?iss.meta=off&iss.only=marketdata&marketdata.columns=CURRENTVALUE';
-const delay = 500;
+const delay = 250;
 let $truraValue;
 
 const delayFetch = (url, options) =>
-    new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(fetch(url, options));
+    new Promise(resolve => {
+        setTimeout(_ => {
+            resolve(fetch(url));
         }, options.delay);
     });
 
-const truraUpdate = (value) => {
+const truraUpdate = value => {
     $truraValue.dataset.value = value || 'Н/Д';
     delayFetch(url, {
-        delay: delay,
-        method: "GET",
-    }).then((response) => response.json())
-        .then((json) => {
+        delay: delay
+    }).then(response => response.json())
+        .then(json => {
             truraUpdate(json.marketdata.data[0][0].toFixed(4));
-        }).catch(e => {
-        truraUpdate();
-    })
+        })
+        .catch(e => {
+            truraUpdate();
+        });
 }
 
-const observer = new MutationObserver(function (mutations, mutationInstance) {
+const observer = new MutationObserver((mutations, mutationInstance) => {
     const $insertAfter = document.querySelector('#marquee-search');
-    if ($insertAfter) {
-        $insertAfter.insertAdjacentHTML('beforebegin', '<div id="trur-inav-widget">TRURA:&nbsp;</div>');
-        $truraValue = document.querySelector('#trur-inav-widget');
-        truraUpdate();
-        mutationInstance.disconnect()
-    }
+    if (!$insertAfter) return;
+    $insertAfter.insertAdjacentHTML('beforebegin', '<div id="trur-inav-widget">TRURA:&nbsp;</div>');
+    $truraValue = document.querySelector('#trur-inav-widget');
+    truraUpdate();
+    mutationInstance.disconnect()
 });
 
 observer.observe(document, {
